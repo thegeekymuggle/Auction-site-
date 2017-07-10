b4680a22fe0e6d16943b881e206f6edbc3cc4920
thegeekymuggle <shashankcr97@gmail.com>
2017-07-10 18:32:41 +0530

    Update admin.py

--- project2/auction/admin.py
+++ project2/auction/admin.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from .models import category, product, final
+from .models import category, product
 from django.contrib import admin
 
 # Register your models here.
 
 admin.site.register(category)
 admin.site.register(product)
-admin.site.register(final)
